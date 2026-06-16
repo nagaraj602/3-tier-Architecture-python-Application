865768f2d15ac60a007a8a18a597cb127d72f6ae
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-06-16 20:36:33 +0530

    Delete Terraform directory

--- Terraform/internet-gateway.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.three_tier_vpc.id
-
-  tags = {
-    Name = "3-tier-igw"
-  }
-}
--- Terraform/vpc.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_vpc" "three_tier_vpc" {
-  cidr_block           = "192.168.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "3-tier VPC"
-  }
-}
