de962b7936bc0473c5574a605c1a30d369399c5f
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-06-16 20:55:16 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "ami_id" {
   default = "ami-080d2f9cbbab903ef"    
-# Above AMI id had the application setup already. If you have deleted this ami. Then create everything with amazon linux default ami: ami-0521cb2d60cfbb1a6  and then install app and then replace the ami.
+# Above AMI id had the application setup already. If you have deleted this ami. Then create everything with amazon linux default ami: ami-0521cb2d60cfbb1a6  and then install app and then replace the ami. After this, you need to run terraform apply command and before that, you need to terminate the existing instance.
 }
 variable "db_username" {
   description = "RDS master username"
